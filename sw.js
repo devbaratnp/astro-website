a7dd204f29f0f6ce8a215eac7efc4da4b7b2563e
PAtel18751 <mind59024@gmail.com>
2026-07-17 14:46:07 +0545

    Publish homepage astrology hub bundle

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='astroshreehari-v6';
+const CACHE='astroshreehari-v7';
 const SHELL=['/','/about','/services','/appointment','/contact','/kundali','/pooja','/panchang','/payment','/manifest.json','/assets/shreehari-logo.webp','/assets/shreehari-icon-192.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(SHELL)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
