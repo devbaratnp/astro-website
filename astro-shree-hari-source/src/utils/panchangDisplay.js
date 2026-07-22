3d9570985d644f9d48a7cede0f13c1badf6d2316
PAtel18751 <mind59024@gmail.com>
2026-07-22 18:24:19 +0545

    Add Panchang day and night forecasts

--- astro-shree-hari-source/src/utils/panchangDisplay.js
+++ astro-shree-hari-source/src/utils/panchangDisplay.js
@@ -1,8 +1,14 @@
 export function buildPanchangRows(panchang = {}) {
   return [
-    ['तिथि', panchang.tithi], ['सूर्योदय', panchang.sunrise], ['सूर्यास्त', panchang.sunset], ['नक्षत्र', panchang.nakshatra], ['करण', panchang.karana], ['योग', panchang.yoga],
+    ['\u0924\u093f\u0925\u093f', panchang.tithi],
+    ['\u0938\u0942\u0930\u094d\u092f\u094b\u0926\u092f', panchang.sunrise],
+    ['\u0938\u0942\u0930\u094d\u092f\u093e\u0938\u094d\u0924', panchang.sunset],
+    ['\u0928\u0915\u094d\u0937\u0924\u094d\u0930', panchang.nakshatra],
+    ['\u0915\u0930\u0923', panchang.karana],
+    ['\u092f\u094b\u0917', panchang.yoga],
   ].filter(([, value]) => value);
 }
+
 export function buildForecastEntries(items = [], period) {
   return items
     .map((item) => ({
--- astro-shree-hari-source/src/utils/panchangDisplay.test.js
+++ astro-shree-hari-source/src/utils/panchangDisplay.test.js
@@ -3,12 +3,11 @@ import assert from 'node:assert/strict';
 import { buildForecastEntries, buildPanchangRows } from './panchangDisplay.js';
 
 test('buildPanchangRows keeps only available Panchang values in display order', () => {
-  assert.deepEqual(buildPanchangRows({ tithi: 'अष्टमी', sunrise: '05:21:31', sunset: '18:58:53', nakshatra: 'स्वाती', yoga: 'साध्य', karana: 'बव' }), [
-    ['तिथि', 'अष्टमी'], ['सूर्योदय', '05:21:31'], ['सूर्यास्त', '18:58:53'], ['नक्षत्र', 'स्वाती'], ['करण', 'बव'], ['योग', 'साध्य'],
-  ]);
+  assert.deepEqual(buildPanchangRows({ tithi: 'x', sunrise: '05:21:31', sunset: '18:58:53', nakshatra: 'y', yoga: 'z', karana: 'w' }), [['\u0924\u093f\u0925\u093f', 'x'], ['\u0938\u0942\u0930\u094d\u092f\u094b\u0926\u092f', '05:21:31'], ['\u0938\u0942\u0930\u094d\u092f\u093e\u0938\u094d\u0924', '18:58:53'], ['\u0928\u0915\u094d\u0937\u0924\u094d\u0930', 'y'], ['\u0915\u0930\u0923', 'w'], ['\u092f\u094b\u0917', 'z']]);
 });
+
 test('buildForecastEntries selects the existing day and night horoscope fields', () => {
-  const item = { zodiac_ne: 'मेष', moon_interpretation: 'दिनको फल', remedy_tips: 'रात्रीको उपाय', infeasible_transit_moon: 'सावधानी' };
-  assert.deepEqual(buildForecastEntries([item], 'day'), [{ title: 'मेष', body: 'दिनको फल', note: '' }]);
-  assert.deepEqual(buildForecastEntries([item], 'night'), [{ title: 'मेष', body: 'रात्रीको उपाय', note: 'सावधानी' }]);
+  const item = { zodiac_ne: 'Aries', moon_interpretation: 'day', remedy_tips: 'night', infeasible_transit_moon: 'caution' };
+  assert.deepEqual(buildForecastEntries([item], 'day'), [{ title: 'Aries', body: 'day', note: '' }]);
+  assert.deepEqual(buildForecastEntries([item], 'night'), [{ title: 'Aries', body: 'night', note: 'caution' }]);
 });
